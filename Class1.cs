bd46d7d7024b4e950e6cd4dd2474916f3c64e74e
for i in range (999) <turkishturk06@gmail.com>
2026-01-16 22:40:48 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static int hp = 30;
+
+    static void game()
+    {
+        Console.WriteLine("a guy comed nearby what you do ?(hit/quit)");
+        string ilkcevap = Console.ReadLine().ToLower();
+
+        if (ilkcevap == "hit")
+        {
+            hp -= 30;
+            Console.WriteLine("you killed the guy");
+        }
+        else if (ilkcevap == "quit")
+        {
+            Console.WriteLine("you quited");
+        }
+        else
+        {
+            Console.WriteLine("invalid pls try again");
+        }
+    }
+
+    static void game()
+    {
+        game();
+    }
+}
